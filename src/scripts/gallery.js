20c590438d0afbc2a7097c8444a2700ba26dedcc
Daniels Jānis Rupenheits <Rogstrixs321@gmail.com>
2024-07-22 18:32:23 +0300

    2

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,53 +1,53 @@
 // JavaScript
 document.querySelectorAll('nav a').forEach(link => {
     link.addEventListener('click', handleClick);
-  });
+});
   
-  function handleClick(event) {
+function handleClick(event) {
     event.preventDefault();
     const target = event.target.getAttribute('href');
     document.querySelector(target).scrollIntoView({ behavior: 'mooth' });
-  }
+}
   
-  const images = document.querySelectorAll('.gallery-container img');
+const images = document.querySelectorAll('.gallery-container img');
   
   images.forEach(image => {
     image.addEventListener('click', handleImageClick);
-  });
+});
   
-  function handleImageClick(event) {
+function handleImageClick(event) {
     const clickedImage = event.target;
     const overlay = createOverlay();
     const imageContainer = createImageContainer();
     const fullScreenImage = createFullScreenImage(clickedImage);
     imageContainer.appendChild(fullScreenImage);
     overlay.appendChild(imageContainer);
-    overlay.className = 'fullscreen-overlay show'; // Add the 'show' class
+    overlay.className = 'fullscreen-overlay show'; // Add the 'how' class
     document.body.appendChild(overlay);
     addZoomFunctionality(fullScreenImage);
     addCloseButton(overlay);
-  }
+}
   
-  function createOverlay() {
+function createOverlay() {
     const overlay = document.createElement('div');
     overlay.className = 'fullscreen-overlay';
     return overlay;
-  }
+}
   
-  function createImageContainer() {
+function createImageContainer() {
     const imageContainer = document.createElement('div');
     imageContainer.className = 'fullscreen-image-container';
     return imageContainer;
-  }
+}
   
-  function createFullScreenImage(image) {
+function createFullScreenImage(image) {
     const fullScreenImage = document.createElement('img');
     fullScreenImage.src = image.src;
     fullScreenImage.alt = image.alt;
     return fullScreenImage;
-  }
+}
   
-  function addZoomFunctionality(image) {
+function addZoomFunctionality(image) {
     let zoomLevel = 1;
     let startX, startY, startZoom;
   
@@ -79,9 +79,9 @@ document.querySelectorAll('nav a').forEach(link => {
         image.style.transform = `scale(${zoomLevel})`;
       }
     }
-  }
+}
   
-  function addCloseButton(overlay) {
+function addCloseButton(overlay) {
     const closeButton = document.createElement('button');
     closeButton.className = 'close-button';
     closeButton.textContent = 'Close';
@@ -90,4 +90,4 @@ document.querySelectorAll('nav a').forEach(link => {
     closeButton.addEventListener('click', () => {
       overlay.remove();
     });
-  }
+}
