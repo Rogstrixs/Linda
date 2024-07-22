7f8e64639b85a1e057a1ef43b07145e7f1646361
Daniels Jānis Rupenheits <Rogstrixs321@gmail.com>
2024-07-22 18:21:06 +0300

    2

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -22,6 +22,7 @@ document.querySelectorAll('nav a').forEach(link => {
     const fullScreenImage = createFullScreenImage(clickedImage);
     imageContainer.appendChild(fullScreenImage);
     overlay.appendChild(imageContainer);
+    overlay.className = 'fullscreen-overlay show'; // Add the 'show' class
     document.body.appendChild(overlay);
     addZoomFunctionality(fullScreenImage);
     addCloseButton(overlay);
